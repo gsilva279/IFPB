52e936bd5e8fcb5e22f6a216ff0530a1efcb653e
Gilvanilson <gilvanilson@localhost.localdomain>
2026-04-19 17:11:09 -0300

    update: atualizando

--- src/introdução à programação/lista_aula/lista_01/atividade_python.py
+++
@@ -1,3 +0,0 @@
-print("===== Soma =====")
-soma = int(input ("Informe um número: ")) + int(input("Informe outro número: "))
-print (f"Soma: {soma}")
---
+++ src/introdução à programação/lista_aula/lista_01/quadrados.py
@@ -0,0 +1,5 @@
+cont = 14
+for i in range(15, 201):
+    quadrados = i * i
+    cont += 1
+    print(f"Quadrado de {cont}: {quadrados}")
